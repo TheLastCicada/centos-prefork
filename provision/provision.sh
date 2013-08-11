d2ba9a87676b537a57a876e523a3e0713edfcda4
Zack Tollman <tollmanz@gmail.com>
2013-08-11 13:25:22 -0700

    Download WordPress.

--- provision/provision.sh
+++ provision/provision.sh
@@ -21,3 +21,55 @@ if [ -f /etc/yum.repos.d/remi.repo ]
 fi
 
 yum install -y httpd mysql-server php php-soap php-pear php-gd php-mbstring php-mcrypt php-mysql php-pecl-apc php-pecl-memcache php-xml vim-enhanced git
+
+# Get WordPress
+wpclone=/usr/local/src/wordpress
+if [ ! -d ${wpclone} ]
+then
+	printf "\nDownloading WordPress from git.....https://github.com/WordPress/WordPress.git\n"
+	git clone https://github.com/WordPress/WordPress.git ${wpclone}
+else
+	printf "\nUpdating WordPress.....\n"
+	cd ${wpclone}
+	git pull
+fi
+
+version=trunk
+site_name=${version}
+location=/var/www/${site_name}
+index_location=${location}/index.php
+config_location=${location}/wp-config.php
+
+printf "\nBuilding ${site_name}.dev"
+if [ ! -f ${index_location} ]
+then
+	printf " * Copying WordPress files for ${site_name}.dev"
+	cp -r ${wpclone} ${location}/
+	if [ ! -f ${index_location} ]
+	then
+		printf "   ...Failed to copy WordPress ${site}.dev files"
+	else
+		printf "   ...${site_name}.dev files copied"
+	fi
+else
+	printf " * Skip setting up files at ${location}, already setup"
+	printf " * Pulling latest core changes to ${location}"
+	cd ${location}
+	git pull
+fi
+
+#if [ ! -f ${config_location} ]
+#then
+#	printf " * Writing ${config_location}"
+#	cd ${location}
+#	wp core config --dbname=${site_name} --dbuser=wp --dbpass=wp --quiet
+#	wp core install --url=${site_name}.dev --quiet --title="${site_name} Dev" --admin_name=admin --admin_email="admin@local.dev" --admin_password="password"
+#	if [ ! -f ${config_location} ]
+#	then
+#		printf "   ...Failed to write ${config_location}"
+#	else
+#		printf "   ...${config_location} written"
+#	fi
+#else
+#	printf " * Skip writing ${config_location}, already written"
+#fi
